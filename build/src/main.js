dd35da88dc5f0b611298cae95a089b546983b7d0
Justin Irabor <justin.irabor@gmail.com>
2022-12-11 16:22:40 +0100

    ChatGPT base

---
+++ build/src/main.js
@@ -0,0 +1,15 @@
+import { ChatGPTAPI } from 'chatgpt';
+import dotenv from 'dotenv';
+dotenv.config();
+async function example(prompt) {
+    const API = new ChatGPTAPI({
+        sessionToken: process.env.SESSION_TOKEN
+    });
+    await API.ensureAuth();
+    const response = await API.sendMessage(`Write a fictional story in the comedy genre based on the following prompt: ${prompt}`, {
+        timeoutMs: 2 * 60 * 1000
+    });
+    console.log(response);
+}
+example('A goat in space');
+//# sourceMappingURL=main.js.map
--- src/main.ts
+++ src/main.ts
@@ -1,34 +1,21 @@
-/**
- * Some predefined delay values (in milliseconds).
- */
-export enum Delays {
-  Short = 500,
-  Medium = 2000,
-  Long = 5000,
+import { ChatGPTAPI } from 'chatgpt';
+import dotenv from 'dotenv';
+dotenv.config();
+
+async function example(prompt: string) {
+    const API = new ChatGPTAPI({
+        sessionToken: process.env.SESSION_TOKEN
+    });
+
+    await API.ensureAuth();
+
+    const response = await API.sendMessage(
+        `Write a fictional story in the comedy genre based on the following prompt: ${prompt}`, {
+            timeoutMs: 2 * 60 * 1000
+        }
+    )
+
+    console.log(response);
 }
 
-/**
- * Returns a Promise<string> that resolves after a given time.
- *
- * @param {string} name - A name.
- * @param {number=} [delay=Delays.Medium] - A number of milliseconds to delay resolution of the Promise.
- * @returns {Promise<string>}
- */
-function delayedHello(
-  name: string,
-  delay: number = Delays.Medium,
-): Promise<string> {
-  return new Promise((resolve: (value?: string) => void) =>
-    setTimeout(() => resolve(`Hello, ${name}`), delay),
-  );
-}
-
-// Please see the comment in the .eslintrc.json file about the suppressed rule!
-// Below is an example of how to use ESLint errors suppression. You can read more
-// at https://eslint.org/docs/latest/user-guide/configuring/rules#disabling-rules
-
-// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
-export async function greeter(name: any) { // eslint-disable-line @typescript-eslint/no-explicit-any
-  // The name parameter should be of type string. Any is used only to trigger the rule.
-  return await delayedHello(name, Delays.Long);
-}
+example('A goat in space');
